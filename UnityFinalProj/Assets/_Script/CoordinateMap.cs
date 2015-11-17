7c0efe1e54ae84dfd669c7a51b14efd592904307
Rolo <rolo_lamperouge@foxmail.com>
2015-11-17 23:01:09 +0800

    add basic coordinate system

---
+++ UnityFinalProj/Assets/_Script/CoordinateMap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoordinateMap : MonoBehaviour {
+//	Vector3 PlayerCoordinate;
+//	Vector3 Obtacle1Coordinate;
+	Vector3 [] Coordinates;
+
+	// Use this for initialization
+	void Start () {
+		Coordinates = new Vector3[Tags.MaxID];
+		Coordinates[Tags.PlayerID] = GameObject.FindGameObjectWithTag (Tags.Player).transform.position;
+		Coordinates[Tags.Obtacle1ID] = GameObject.FindGameObjectWithTag (Tags.Obstacle).transform.position;
+//		PlayerCoordinate = GameObject.FindGameObjectWithTag (Tags.Player).transform.position;
+//		Obtacle1Coordinate = GameObject.FindGameObjectWithTag (Tags.Obstacle).transform.position;
+	}
+
+	public void updateCoordinate(int flag, Vector3 currentPosition){
+
+		switch (flag) {
+		case Tags.PlayerID:
+			Coordinates[Tags.PlayerID] = currentPosition;
+			break;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Coordinates[Tags.PlayerID]);
+	}
+}
---
+++ UnityFinalProj/Assets/_Script/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	float h,v;
+	CoordinateMap map;
+	Vector3 currentPosition;
+	// Use this for initialization
+	void Start () {
+		currentPosition = new Vector3();
+		map = GameObject.FindGameObjectWithTag (Tags.GameController).GetComponent<CoordinateMap> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		h = Input.GetAxis ("Horizontal");
+		v = Input.GetAxis ("Vertical");
+		transform.Translate (h,v,0);
+		currentPosition = transform.position;
+		map.updateCoordinate (Tags.PlayerID, transform.position);
+	}
+}
