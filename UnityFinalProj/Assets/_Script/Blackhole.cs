945e92c473dcc966276a279a121dc7e78c200f7e
Rolo <rolo_lamperouge@foxmail.com>
2015-11-20 06:18:54 +0800

    add new assets and finish basic black hole

---
+++ UnityFinalProj/Assets/_Script/Blackhole.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blackhole : MonoBehaviour {
+	public float gravity = 12000;
+	public GameObject player;
+	const float maxBoundary = 170;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerStay(Collider other){
+		other.gameObject.GetComponent<Rigidbody>().AddForce (other.transform.position * gravity * -1.0f/Mathf.Pow(other.transform.position.magnitude,2));
+
+	}
+	void OnCollisionEnter(Collision other){
+		Debug.Log ("Colllision entered");
+		if (other.collider.gameObject.tag == Tags.Player)
+			Destroy (player);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
