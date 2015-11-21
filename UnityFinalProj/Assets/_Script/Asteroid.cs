7975f421bb004d5b6f3ff708f7320861bd08c06b
Rolo <rolo_lamperouge@foxmail.com>
2015-11-22 03:26:55 +0800

    basic rigid body of asteroids

---
+++ UnityFinalProj/Assets/_Script/Asteroid.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+	public GameObject player;
+	Vector3 rotation;
+	float rotateSpeed;
+	// Use this for initialization
+	void Start () {
+		rotation = 	Random.onUnitSphere;
+		rotateSpeed = Random.Range (0.2f,3.0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rotation * rotateSpeed);
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == Tags.Player) {
+			Debug.Log ("On Collision Enter");
+		}
+	}
+}
---
+++ UnityFinalProj/Assets/_Script/AsteroidField.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidField : MonoBehaviour {
+	public GameObject [] Asteroids;
+	public float 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
