f8f0d95a9cfeb138a87dacc68a78a4c1839b3eb5
Rolo <rolo_lamperouge@foxmail.com>
2015-11-29 07:54:18 +0800

    basic menu scene
    basic menu scene with out function

---
+++ UnityFinalProj/Assets/_Script/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+	public GameObject menu;
+	public Menu CurrentMenu;
+
+	void Awake(){
+		CurrentMenu = menu.GetComponent<Menu> (); 
+		Debug.Log("start");
+		ShowMenu (CurrentMenu);
+	}
+
+	public void ShowMenu(Menu menu){
+		if (CurrentMenu != null)
+			CurrentMenu.IsOpen = false;
+
+		CurrentMenu = menu;
+		CurrentMenu.IsOpen = true;
+	
+	}
+}
